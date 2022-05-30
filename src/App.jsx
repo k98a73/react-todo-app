9d7f1d50dd7ffe9a07871a0411ffcb238bdf5d5a
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-05-30 14:54:41 +0900

    タスクの完了機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,34 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-  const [incomleteTodos, setIncomleteTodos] = useState([
+  const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいい"
   ]);
-  const [comleteTodos, setComleteTodos] = useState(["うううう"]);
+  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
   const onClickAdd = () => {
     if (todoText === "") return;
-    const newTodos = [...incomleteTodos, todoText];
-    setIncomleteTodos(newTodos);
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
     setTodoText("");
   };
 
   const onClickDelete = (index) => {
-    const newTodos = [...incomleteTodos];
+    const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
-    setIncomleteTodos(newTodos);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
   };
 
   return (
@@ -37,11 +46,11 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incomleteTodos.map((todo, index) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
@@ -51,7 +60,7 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {comleteTodos.map((todo, index) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
